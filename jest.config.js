fd249c077289f9f37397ccb3424ded3c21fc5ba4
“Stanislav <w0859305@myscc.ca>
2025-05-25 18:33:48 -0400

    Re-design landing

---
+++ jest.config.js
@@ -0,0 +1,11 @@
+import {createDefaultPreset} from "ts-jest";
+
+const tsJestTransformCfg = createDefaultPreset().transform;
+
+/** @type {import("jest").Config} **/
+export default {
+  testEnvironment: "jsdom",
+  transform: {
+    ...tsJestTransformCfg,
+  },
+};
